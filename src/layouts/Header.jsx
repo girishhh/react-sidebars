c278fa8ee0f38aceba85b62b5ad3b5ed189a7098
girishhhhh <girikulkarni03@gmail.com>
2021-12-24 01:58:12 +0530

    sidebar changes

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,9 +1,11 @@
 import "./Header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
 
 function Header({ toggleSidebar }) {
   return (
-    <div className="header">
-      <button onClick={toggleSidebar}>toggle</button>Header
+    <div className="header d-flex align-items-center">
+      <FontAwesomeIcon  className="ps-2" icon={faSlidersH} onClick={toggleSidebar} size="2x" />
     </div>
   );
 }
--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -1,10 +1,37 @@
 import "./SideBar.scss";
+import LOGO from "../styles/images/whatsapp.svg";
+import IPHONE from "../styles/images/iphone.png";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
+
+const SideNavBarItems = [
+  {
+    path: "test",
+    img: IPHONE,
+    alt: "sidebar.dashboardAltText",
+    title: "sidebar.dashboard",
+  },
+];
 
 function SideBar() {
   return (
     <div className="side-bar h-100">
-      <div className="sidebar-logo">logo</div>
-      <div className="menus">Sidebar</div>
+      <div className="sidebar-logo d-flex justify-content-center align-items-center">
+        <img src={LOGO} className="logo" alt="logo" />
+      </div>
+
+      <Nav className="flex-column">
+        {SideNavBarItems.map((item) => (
+          <NavItem key={item.path} className="mb-3">
+            <NavLink
+              to={item.path}              
+              className="p-0"
+            >
+              <img src={item.img} alt={item.alt} className="side-bar-icon" />
+              {item.title}
+            </NavLink>
+          </NavItem>
+        ))}
+      </Nav>
     </div>
   );
 }
