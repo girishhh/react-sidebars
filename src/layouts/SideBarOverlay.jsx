6f1bf4d75580cb1bbb99c3710e9e9a725c83b287
girishhhhh <girikulkarni03@gmail.com>
2021-12-27 17:33:17 +0530

    alt change

--- src/layouts/SideBarOverlay.jsx
+++ src/layouts/SideBarOverlay.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { useState } from "react";
+import React from "react";
 import { Nav, Offcanvas } from "react-bootstrap";
 import MenuItem from "../components/MenuItem";
 import { SideNavBarItems } from "../NavBarConstants";
