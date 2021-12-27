57544efbe3042861c2981bb21f81316a89877597
girishhhhh <girikulkarni03@gmail.com>
2021-12-27 15:54:58 +0530

    static host

--- src/components/Dashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Dashboard() {
-  return <>DASHBOARD</>;
-}
-
-export default Dashboard;
--- src/components/Notification.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Notification() {
-  return <>NOTIFICATION</>;
-}
-
-export default Notification;
